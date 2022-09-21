bb88b557318188d274d2f359a8156f1b82db9491
Wahyu <wahyukoco535@gmail.com>
2022-09-21 17:07:43 +0700

    splash screen

--- app/src/main/java/com/example/wahyustoryapp/MainActivity.kt
+++ app/src/main/java/com/example/wahyustoryapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wahyustoryapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
 import com.example.wahyustoryapp.data.auth.AuthPreference
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
