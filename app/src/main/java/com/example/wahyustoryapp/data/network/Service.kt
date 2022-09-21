797b67974fddd50aab7b7e33537c2537c5d97e7f
Wahyu <wahyukoco535@gmail.com>
2022-09-22 00:31:59 +0700

    refactor: file structure

---
+++ app/src/main/java/com/example/wahyustoryapp/data/network/Service.kt
@@ -0,0 +1,58 @@
+package com.example.wahyustoryapp.data.network
+
+import com.example.wahyustoryapp.BuildConfig
+import com.example.wahyustoryapp.data.network.response.LoginResponse
+import com.example.wahyustoryapp.data.network.response.NormalResponse
+import com.example.wahyustoryapp.data.network.response.StoryResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+interface ApiService {
+
+    @Multipart
+    @POST
+    fun postData()
+
+    @POST("login")
+    suspend fun getLoginData(
+        @Body form: LoginForm
+    ): Response<LoginResponse>
+
+    @POST("register")
+    suspend fun registerUser(
+        @Body form: RegisterForm
+    ): Response<NormalResponse>
+
+    @GET("stories")
+    suspend fun getAllStory(
+        @Header("Authorization") token: String,
+        @Query("page") page: Int = 1,
+        @Query("size") size: Int?,
+        @Query("location") location: Int?
+    ): Response<StoryResponse>
+}
+
+object ApiConfig {
+    fun getApiService(): ApiService {
+        val baseUrl = "https://story-api.dicoding.dev/v1/"
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = run {
+            if (BuildConfig.DEBUG) {
+                OkHttpClient.Builder().addInterceptor(interceptor).build()
+            } else {
+                OkHttpClient.Builder().build()
+            }
+        }
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
+
