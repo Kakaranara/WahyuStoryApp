3c43e3a5b03a352e943880eebd3656e182579ab0
Wahyu <wahyukoco535@gmail.com>
2022-09-21 12:36:48 +0700

    adding comment

--- app/src/main/java/com/example/wahyustoryapp/MainActivity.kt
+++ app/src/main/java/com/example/wahyustoryapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.wahyustoryapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
 import com.example.wahyustoryapp.data.auth.AuthPreference
@@ -29,31 +28,18 @@ class MainActivity : AppCompatActivity() {
         val job = Job()
         val myScope = CoroutineScope(Dispatchers.Main + job)
 
+        //kode ini rentan terhadap configuration change
+        //mohon sarannya untuk membuat start destionation graph dengan lebih baik
         myScope.launch(Dispatchers.Main) {
+            //checking login async
             authPrefs.isLogin().collect {
                 if (it) {
                     navGraph.setStartDestination(R.id.homeFragment)
                 } else {
                     navGraph.setStartDestination(R.id.loginFragment)
                 }
+                //set navGraph with determined start destination
                 navHost.navController.graph = navGraph
-            }
-        }
-    }
-
-    suspend fun setupStartDestionation(){
-        val navHost = supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment
-        val navGraph: NavGraph = navHost.navController.navInflater.inflate(R.navigation.main_nav)
-        val authPrefs = AuthPreference.getInstance(authDataStore)
-
-        val job = Job()
-        val myScope = CoroutineScope(Dispatchers.Main + job)
-
-        authPrefs.isLogin().collect{
-            if(it){
-                navGraph.setStartDestination(R.id.homeFragment)
-            }else{
-                navGraph
             }
         }
     }
