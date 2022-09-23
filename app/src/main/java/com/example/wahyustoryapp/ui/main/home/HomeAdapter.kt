2140dfd695ac89b82595c45de088aaf93fde5587
Wahyu <wahyukoco535@gmail.com>
2022-09-23 20:05:01 +0700

    change card style

--- app/src/main/java/com/example/wahyustoryapp/ui/main/home/HomeAdapter.kt
+++ app/src/main/java/com/example/wahyustoryapp/ui/main/home/HomeAdapter.kt
@@ -6,14 +6,15 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.example.wahyustoryapp.data.database.Story
+import com.example.wahyustoryapp.databinding.ListItem2Binding
 import com.example.wahyustoryapp.databinding.ListItemBinding
 
 class HomeAdapter(private val listItem: List<Story>) :
     RecyclerView.Adapter<HomeAdapter.ListViewHolder>() {
-    class ListViewHolder(val binding: ListItemBinding) : ViewHolder(binding.root)
+    class ListViewHolder(val binding: ListItem2Binding) : ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = ListItem2Binding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ListViewHolder(view)
     }
 
@@ -21,13 +22,13 @@ class HomeAdapter(private val listItem: List<Story>) :
         val data = listItem[position]
         holder.apply {
             binding.apply {
-                itemTitle.text = data.name
-                itemDescription.text = data.description
+                itemTitle2.text = data.name
+                itemDescription2.text = data.description
             }
         }
         Glide.with(holder.binding.root.context)
             .load(data.photoUrl)
-            .into(holder.binding.storyImage)
+            .into(holder.binding.storyImage2)
     }
 
     override fun getItemCount(): Int = listItem.size
