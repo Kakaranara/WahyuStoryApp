89c9e1b876c6006b31d458a111522d8e4e641cdd
Wahyu <wahyukoco535@gmail.com>
2022-09-24 09:45:03 +0700

    feat: adding documentation

--- app/src/main/java/com/example/wahyustoryapp/MainActivity.kt
+++ app/src/main/java/com/example/wahyustoryapp/MainActivity.kt
@@ -7,6 +7,13 @@ import com.example.wahyustoryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * This project use single-activity architecture.
+     * Check the res > nav
+     * This project also use offline-online pattern.
+     * Making this project available when offline.
+     */
+
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -16,28 +23,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
-
-//        val navHost = supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment
-//        val navGraph: NavGraph = navHost.navController.navInflater.inflate(R.navigation.main_nav)
-//        val authPrefs = AuthPreference.getInstance(authDataStore)
-//
-//        val job = Job()
-//        val myScope = CoroutineScope(Dispatchers.Main + job)
-//
-//        //kode ini rentan terhadap configuration change
-//        //mohon sarannya untuk membuat start destionation graph dengan lebih baik
-//        myScope.launch(Dispatchers.Main) {
-//            //checking login async
-//            authPrefs.isLogin().collect {
-//                if (it) {
-//                    navGraph.setStartDestination(R.id.homeFragment)
-//                } else {
-//                    navGraph.setStartDestination(R.id.loginFragment)
-//                }
-//                //set navGraph with determined start destination
-//                navHost.navController.graph = navGraph
-//            }
-//        }
     }
 }
