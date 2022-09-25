4e1b889e25b4334f900308637a1adeb8c584988a
Wahyu <wahyukoco535@gmail.com>
2022-09-26 00:18:23 +0700

    Localization success

--- app/src/main/java/com/example/wahyustoryapp/ui/SettingFragment.kt
+++ app/src/main/java/com/example/wahyustoryapp/ui/SettingFragment.kt
@@ -1,6 +1,8 @@
 package com.example.wahyustoryapp.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -29,6 +31,11 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
+        binding.btnChangeLanguage.setOnClickListener{
+            Intent(Settings.ACTION_LOCALE_SETTINGS).also {
+                startActivity(it)
+            }
+        }
     }
 
     private fun setupToolbar(){
