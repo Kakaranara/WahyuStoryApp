f28a93a07fe71b602b57d0a745a15b3dc7f573ba
Wahyu <wahyukoco535@gmail.com>
2022-10-18 14:13:34 +0700

    refactoring again

--- app/src/main/java/com/example/wahyustoryapp/ui/auth/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/wahyustoryapp/ui/auth/register/RegisterViewModel.kt
@@ -9,19 +9,10 @@ import com.example.wahyustoryapp.data.network.RegisterForm
 import com.example.wahyustoryapp.data.network.response.NormalResponse
 import com.example.wahyustoryapp.helper.Async
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import retrofit2.Response
 
 class RegisterViewModel : ViewModel() {
-
-
-//    private var _isLoading: MutableLiveData<Boolean> = MutableLiveData()
-//    val isLoading: LiveData<Boolean> get() = _isLoading
-//
-//    private var _message: MutableLiveData<String> = MutableLiveData()
-//    val message: LiveData<String> get() = _message
-//
-//    private var _isRegisterSuccess: MutableLiveData<Boolean> = MutableLiveData()
-//    val isRegisterSucces: LiveData<Boolean> get() = _isRegisterSuccess
 
     private val _registerEvent: MutableLiveData<Async<Response<NormalResponse>>> = MutableLiveData()
     val registerEvent: LiveData<Async<Response<NormalResponse>>> get() = _registerEvent
@@ -34,38 +25,15 @@ class RegisterViewModel : ViewModel() {
                 if (response.isSuccessful) {
                     _registerEvent.postValue(Async.Success(response))
                 } else {
-                    _registerEvent.postValue(Async.Error(response.message()))
+                    response.errorBody()?.let {
+                        val error = JSONObject(it.string())
+                        _registerEvent.postValue(Async.Error(error.getString("message")))
+                    }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
                 _registerEvent.postValue(Async.Error("Terjadi suatu masalah"))
             }
-//            _isLoading.value = true
-//            val response = ApiConfig
-//                .getApiService()
-//                .registerUser(form)
-//
-//            if (response.isSuccessful) {
-//                _isLoading.value = false
-//                response.body()?.let {
-//                    _message.value = it.message
-//                    _isRegisterSuccess.value = true
-//                }
-//                _registerEvent.postValue(Async.Success(response))
-//            } else {
-//                _isLoading.value = false
-//                _isRegisterSuccess.value = false
-//                response.errorBody()?.let {
-//                    try {
-//                        val obj = JSONObject(it.string())
-//                        _message.value = obj.getString("message")
-//
-//                    } catch (e: Exception) {
-//                        _message.value = "Terjadi kesalahan pada server"
-//                    }
-//
-//                }
-//            }
         }
     }
 }
