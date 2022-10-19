063332c93cd23c10e8c2c59be3416c2e0cc95aca
Wahyu <wahyukoco535@gmail.com>
2022-10-19 18:16:06 +0700

    just a fun things

--- app/src/main/java/com/example/wahyustoryapp/sandbox.kt
+++ app/src/main/java/com/example/wahyustoryapp/sandbox.kt
@@ -1,5 +1,26 @@
 package com.example.wahyustoryapp
 
 fun main() {
+    val counter = Counter()
+    val wrapper = CounterRepository(counter)
+    val c= Counter()
 
+    wrapper.increment()
+    counter.increment()
+    println(counter.a)
+    println(wrapper.counter.a)
+    println(c.a)
+}
+
+class Counter() {
+    var a = 5
+    fun increment() {
+        a++
+    }
+}
+
+class CounterRepository(var counter: Counter) {
+    fun increment() {
+        counter.increment()
+    }
 }
