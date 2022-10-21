ac9097792d5eda545822678c97d1d0df375f143a
Wahyu <wahyukoco535@gmail.com>
2022-10-21 21:19:24 +0700

    maps view model ( i need it later i guess..)

---
+++ app/src/main/java/com/example/wahyustoryapp/ui/main/maps/MapsViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.wahyustoryapp.ui.main.maps
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.wahyustoryapp.data.network.ApiConfig
+import kotlinx.coroutines.launch
+
+class MapsViewModel(private val token: String) : ViewModel() {
+    fun requestApisLocation() {
+        viewModelScope.launch {
+            try {
+
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+}
