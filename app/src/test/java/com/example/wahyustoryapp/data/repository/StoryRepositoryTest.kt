681fc44fc63b6093c00f8a19048eedbe5b360104
Wahyu <wahyukoco535@gmail.com>
2022-11-02 17:17:20 +0700

    story repository test

--- app/src/test/java/com/example/wahyustoryapp/data/repository/StoryRepositoryTest.kt
+++ app/src/test/java/com/example/wahyustoryapp/data/repository/StoryRepositoryTest.kt
@@ -1,25 +1,33 @@
 package com.example.wahyustoryapp.data.repository
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.RoomDatabase
+import com.example.wahyustoryapp.DataDummy
 import com.example.wahyustoryapp.MainDispatcherRule
+import com.example.wahyustoryapp.constant.Constant
+import com.example.wahyustoryapp.data.database.RemoteKeysDao
 import com.example.wahyustoryapp.data.database.StoryDao
 import com.example.wahyustoryapp.data.database.StoryRoomDatabase
 import com.example.wahyustoryapp.data.fake.FakeApiStory
+import com.example.wahyustoryapp.data.fake.FakeRemoteKeysDao
+import com.example.wahyustoryapp.data.fake.FakeStoryDao
 import com.example.wahyustoryapp.data.network.ApiService
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
 import java.io.File
 
-//@RunWith(MockitoJUnitRunner::class)
+@RunWith(MockitoJUnitRunner::class)
 @ExperimentalCoroutinesApi
 class StoryRepositoryTest {
 
@@ -29,27 +37,67 @@ class StoryRepositoryTest {
     @get:Rule
     val disp = MainDispatcherRule()
 
-    @Mock
-    private lateinit var dao : StoryDao
 
-    @Mock
-    private lateinit var database: StoryRoomDatabase
     private lateinit var api: ApiService
-    private val token = ""
+    private lateinit var database: StoryRoomDatabase
     private lateinit var repository: StoryRepository
-
-    @Mock
-    private lateinit var repoMock : StoryRepository
+    private val token = ""
+    private lateinit var dao: StoryDao
+    private lateinit var remoteDao: RemoteKeysDao
 
     @Before
     fun setup() {
+        database = mock(StoryRoomDatabase::class.java)
         api = FakeApiStory()
+        dao = FakeStoryDao()
+        remoteDao = FakeRemoteKeysDao()
         repository = StoryRepository(database, api, token)
+
+        // * hal ini harus dilakukan, karena bila tidak database akan mengeluarkan :
+        // ! null pointer exception! invoke suspend!
+        // ? saya juga kurang tahu sebenarnya kenapa ini bisa terjadi.
+        // ? mungkin karena yang di mock abstract class?
+        `when`(database.storyDao()).thenReturn(dao)
+        `when`(database.remoteKeysDao()).thenReturn(remoteDao)
     }
 
-//    @Test
-//    fun `test clearing db`() = runTest{
-//        `when`(dao.deleteAll())
-//        repository.clearDb()
-//    }
+    @Test
+    fun `refreshed db size should be the same with constant SIZE_FOR_REFRESH`() = runTest {
+        val dummy = DataDummy.provideStoryList()
+        repeat(3) { //? just to simulate db operating
+            database.storyDao().insertAll(dummy)
+        }
+
+        repository.refreshRepositoryData()
+        Assert.assertNotNull(database.storyDao().getAllStoriesValues().size)
+        Assert.assertEquals(
+            database.storyDao().getAllStoriesValues().size,
+            Constant.SIZE_FOR_REFRESH
+        )
+    }
+
+    @Test
+    fun `when latLng is null, api call uploadImage`()  = runTest{
+        val mockFile = mock(File::class.java)
+        repository.addStory(mockFile, "", null)
+
+        Assert.assertTrue((api as FakeApiStory).isUploadImageTriggered)
+    }
+
+    @Test
+    fun `when latLng is not null, api call uploadImageWithLocation`()  = runTest{
+        val mockFile = mock(File::class.java)
+        repository.addStory(mockFile, "", LatLng(1.0,1.0))
+
+        Assert.assertTrue((api as FakeApiStory).isUploadImageWithLocationTriggered)
+    }
+
+    @Test
+    fun `test clearing db`() = runTest {
+        val dummy = DataDummy.provideStoryList()
+        database.storyDao().insertAll(dummy)
+        repository.clearDb()
+
+        Assert.assertTrue(database.storyDao().getAllStoriesValues().isEmpty())
+    }
 }
